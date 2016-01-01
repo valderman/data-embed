9a7e18fa77b4c35b5e8622c85a8e01ba756d3bb5
Anton Ekblad <anton@ekblad.cc>
2016-01-01 13:18:43 +0100

    Fix docs + error messages for Data.Embed.

--- Data/Embed.hs
+++ Data/Embed.hs
@@ -12,23 +12,30 @@ import System.Environment.Executable
 import Data.Embed.File
 
 {-# NOINLINE myBundle #-}
--- | Handle to this application's bundle. Throws an error upon evaluation if
---   the application has no bundle.
+-- | Handle to this application's embedded file bundle.
+--   Throws an error upon evaluation if the application has none.
 myBundle :: Bundle
 myBundle =
   case unsafePerformIO (getExecutablePath >>= openBundle) of
     Right b -> b
     Left e  -> error $ "couldn't open executable's bundle: " ++ e
 
--- | Returns the specified embedded file, or Nothing if it does not exist.
+-- | Returns the specified embedded file, or Nothing if it does not exist or
+--   this executable has no embedded file bundle.
 embeddedFile :: FilePath -> Maybe BS.ByteString
 embeddedFile fp =
-  case unsafePerformIO (readBundleFile myBundle fp) of
-    Right bytes -> Just bytes
-    _           -> Nothing
+    case theFile of
+      Right bytes -> Just bytes
+      _           -> Nothing
+  where
+    theFile = unsafePerformIO $ do
+      eb <- getExecutablePath >>= openBundle
+      case eb of
+        Right b -> readBundleFile b fp
+        Left e  -> return (Left e)
 
--- | Like 'bundledFile', but throws an error if the given file does not exist
---   in the executable's bundle.
+-- | Like 'embeddedFile', but throws an error if the given file does not exist
+--   in the executable's embedded file bundle.
 embeddedFile' :: FilePath -> BS.ByteString
 embeddedFile' fp =
-  maybe (error $ "no such bundled file: `" ++ fp ++ "'") id $ embeddedFile fp
+  maybe (error $ "no such embedded file: `" ++ fp ++ "'") id $ embeddedFile fp
